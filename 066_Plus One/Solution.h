b44e914a009fffcfba9749299cc56e2f70271e03
liuxi93ing <liuxi93ing@gmail.com>
2017-12-05 01:00:48 -0700

    Q66 unfinished

---
+++ 066_Plus One/Solution.h
@@ -0,0 +1,32 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int i;
+        int flag = 1,allNine = 1;
+        for(i=digits.size()-1;i>=0;i--)
+        {
+            if(digits[i]==9&&flag==1)
+                digits[i]=0;
+            else if(digits[i]!=9)
+            {
+                digits[i]+= flag;
+                flag = 0;
+            }
+            if(digits[i]!=9)
+                allNine = 0;
+        }
+        if(!allNine)
+            return digits;
+        else
+        {
+            vector<int> new_digits(1,1);
+            for(i=0;i<digits.size();i++)
+                new_digits.push_back(0);
+            return new_digits;
+        }
+    }
+};
--- 066_Plus One/main.cpp
+++ 066_Plus One/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <vector>
+#include "Solution.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Solution s;
+    vector<int> nums = {9};
+    vector<int> nums_2 = s.plusOne(nums);
+    for(auto i = nums_2.begin();i!=nums_2.end();i++)
+        cout<<*i;
     return 0;
 }
