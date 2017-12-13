278768bf4b7864ccf942096b995150c85cc5c9ba
liuxi93ing <liuxi93ing@gmail.com>
2017-12-12 22:31:54 -0700

    Q100 attempted

--- 100_Same Tree/Main.cpp
+++ 100_Same Tree/Main.cpp
@@ -3,35 +3,24 @@
 
 using namespace std;
 
-TreeNode* create_Tree(int a[], int len)
-{
-    return root;
-}
-
-void print_Tree(TreeNode* root)
-{
-
-}
-
-
 int main()
 {
     Solution s;
 
-    int a[] = {1,2,3,4,5};
-    int b[] = {1,2,3,4,5};
-    int len_a = sizeof(a)/sizeof(a[0]);
-    int len_b = sizeof(b)/sizeof(b[0]);
-    TreeNode* root1;
-    TreeNode* root2;
+    int pre_a[] = {1,2,4,5,6,7,3};
+    int in_a[] = {4,2,6,5,7,1,3};
+    int pre_b[] = {1,2,4,5,6,7,3};
+    int in_b[] = {4,2,6,5,7,1,3};
+    int len_a = sizeof(pre_a)/sizeof(pre_a[0]);
+    int len_b = sizeof(pre_b)/sizeof(pre_b[0]);
+    TreeNode* root_a = create_Tree(pre_a,in_a,len_a);
+    TreeNode* root_b = create_Tree(pre_b,in_b,len_b);
 
-    root1 = create_Tree(a,len_a);
-    root2 = create_Tree(b,len_b);
-
-    print_Tree(root1);
-    print_Tree(root2);
-
-
+    pre_order_print(root_a);
     cout<<endl;
+    pre_order_print(root_b);
+    cout<<endl;
+
+    cout<<s.isSameTree(root_a,root_b);
     return 0;
 }
