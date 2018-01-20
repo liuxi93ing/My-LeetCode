8cd71a16c13e65db7033834c173a0d187f486f65
liuxi93ing <liuxi93ing@gmail.com>
2018-01-19 19:49:26 -0700

    pre update

--- 110_Balanced Binary Tree/README.md
+++ 110_Balanced Binary Tree/README.md
@@ -5,7 +5,7 @@ Given a binary tree, determine if it is height-balanced.
 For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
 
 
+# 解析：
 
-
-
+判断一颗树是否是平衡二叉树要判断两点：1、左右子树都是平衡二叉树。2、左右子树高度差小于1。
 
--- 111_Minimum Depth of Binary Tree/Solution.h
+++ 111_Minimum Depth of Binary Tree/Solution.h
@@ -1,10 +1,31 @@
 #include "TreeNode.h"
 using namespace std;
 
+// my solution
 class Solution{
 public:
     int minDepth(TreeNode* root) {
+        if(!root) return 0;
+        if(root->left==NULL&&root->right==NULL)
+            return 1;
 
+        int left_depth = 10000;
+        int right_depth = 10000;
+        if(root->left!=NULL)
+            left_depth = minDepth(root->left);
+        if(root->right!=NULL)
+            right_depth = minDepth(root->right);
+        return (left_depth < right_depth ? left_depth: right_depth) + 1;
+    }
+};
 
+// more clean code
+class Solution{
+public:
+    int minDepth(TreeNode* root){
+        if(root==NULL) return 0;
+        if(root->left==NULL) return minDepth(root->right)+1;
+        if(root->right==NULL) return minDepth(root->left)+1;
+        return min(minDepth(root->left),minDepth(root->right))+1;
     }
 };
