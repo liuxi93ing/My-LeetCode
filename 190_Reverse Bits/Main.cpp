cf096189e4c19b21e30655172378d37ad4b1d9f5
liuxi93ing <liuxi93ing@gmail.com>
2018-02-08 22:44:21 -0700

    Q190 added

--- 190_Reverse Bits/Main.cpp
+++ 190_Reverse Bits/Main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <cstdint>
 #include "Solution.h"
 
 using namespace std;
 
 int main()
 {
-    Solution s;
-    cout<<endl;
-    return ;
+    Solution_3 s;
+    uint32_t val = 1;
+    cout<<endl<<s.reverseBits(val)<<endl;
+    return 0;
 }
--- 190_Reverse Bits/Solution.h
+++ 190_Reverse Bits/Solution.h
@@ -1,8 +1,52 @@
-
+#include <cstdint>
+#include <stack>
+#include <bitset>
 using namespace std;
 
-class Solution{
+class Solution_1{
 public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res = 0;
+        stack<int> sta;
+        int factor = 1;
+        while(n!=0) {
+            sta.push(n%2);
+            n /= 2;
+        }
+        while(sta.size()!=32)
+            sta.push(0);
 
+        while(!sta.empty()){
+            res += sta.top()*factor;
+            factor *= 2;
+            cout<<sta.top();
+            sta.pop();
+        }
+        return res;
+    }
 
+};
+
+class Solution_2{
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res = 0;
+        for(int i=0; i<32; i++) {
+            res = res<<1;
+            if(n&1) res = res|1;
+            n = n>>1;
+        }
+        return res;
+    }
+};
+
+class Solution_3{
+public:
+    uint32_t reverseBits(uint32_t n) {
+        bitset<32> x(n);
+        bitset<32> y;
+        for(int i=0;i<20;i++)
+            y[31-i] = x[i];
+        return y.to_ulong();
+    }
 };
