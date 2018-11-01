6fcc3510eb70c1291a13f2cb916657d211d19f37
liuxi93ing <liuxi93ing@gmail.com>
2018-10-31 23:01:56 -0600

    readme update

--- 198_House Robber/Main.cpp
+++ 198_House Robber/Main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    Solution s;
+    Solution s_1;
     vector<int> data = {1,2,5,3,8,1,4,9};
     cout<<s.rob(data)<<endl;
     return 0;
--- 198_House Robber/Solution.h
+++ 198_House Robber/Solution.h
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-class Solution{
+class Solution_1{
 public:
     int rob(vector<int>& nums) {
         int a = 0;
@@ -14,6 +14,24 @@ public:
                 b = max(b+nums[i],a);
         }
         return max(a,b);
+    }
+
+};
+
+// 动态规划的思想
+class Solution_2{
+public:
+    int rob(vector<int>& nums) {
+        if(nums.size()==0) return 0;
+        if(nums.size()==1) return nums[0];
+
+        //使用f[n] 来记录从第1家到第n家最大收益
+        vector<int> f(nums.size(),0);
+        f[0] = nums[0];
+        f[1] = max(nums[0], nums[1]);
+        for(int i=2; i<nums.size(); i++)
+            f[i] = max(f[i-1], f[i-2]+nums[i]);  // 状态转移
+        return f[nums.size()-1];
     }
 
 };
